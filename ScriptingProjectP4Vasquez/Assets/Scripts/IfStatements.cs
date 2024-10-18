d725144852dbe33e4d353ff1bacca89de7e6a29b
JVgamer717 <717652@student.dusd.net>
2024-10-18 12:14:24 -0700

    create an if statement script
    I created an if statement script and completed it

---
+++ ScriptingProjectP4Vasquez/Assets/Scripts/IfStatements.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    float coffeeTemperature = 85.0f;
+    float hotLimitTempature = 70.0f;
+    float coldLimitTempature = 40.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            TemperatureTest();
+
+        coffeeTemperature -= Time.deltaTime * 5f;
+    }
+    void TemperatureTest()
+    {
+
+        if (coffeeTemperature > hotLimitTempature) 
+        {
+
+            print("Coffe is too hot.");
+        }
+
+        else if (coffeeTemperature < coldLimitTempature)
+        {
+
+            print("Coffe is too cold.");
+        }
+
+        else
+        {
+
+            print("Coffee is just right.");
+        }
+    }
+}
